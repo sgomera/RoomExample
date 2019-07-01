9e96fbd9fbdb5df9317180a6ffaa5f447f1266c8
sgomera <silviagomera@gmail.com>
2019-07-01 12:55:27 +0200

    Initial commit

---
+++ app/src/main/java/com/example/roomexample/MyDao.java
@@ -0,0 +1,13 @@
+package com.example.roomexample;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+
+@Dao
+public interface MyDao {
+
+    @Insert
+    public void addUser (User user);
+
+
+}
