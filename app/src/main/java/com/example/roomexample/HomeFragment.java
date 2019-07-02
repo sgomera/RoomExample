57c49308ae0b028ae8b7a4b78a4504a6a9b96aa8
sgomera <silviagomera@gmail.com>
2019-07-02 10:35:28 +0200

    Using Fragments

--- app/src/main/java/com/example/roomexample/HomeFragment.java
+++ app/src/main/java/com/example/roomexample/HomeFragment.java
@@ -3,16 +3,19 @@ package com.example.roomexample;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends Fragment {
-
+public class HomeFragment extends Fragment implements View.OnClickListener {
+    private Button BnAddUser;
+    FragmentTransaction myTransaction;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -23,7 +26,32 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        BnAddUser = view.findViewById(R.id.bn_add_user);
+        BnAddUser.setOnClickListener(this);
+
+        myTransaction = MainActivity.fragmentManager.beginTransaction();
+
+
+        return view;
+
+
+
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (getView().getId()){
+            case R.id.bn_add_user:
+                /*MainActivity.fragmentManager.beginTransaction().
+                        replace(R.id.fragment_container, new AddUserFragment()).
+                        addToBackStack(null).commit();*/
+                myTransaction.replace(R.id.fragment_container, new AddUserFragment());
+                myTransaction.commit();
+
+                break;
+        }
+
+    }
 }
--- app/src/main/java/com/example/roomexample/MainActivity.java
+++ app/src/main/java/com/example/roomexample/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.roomexample;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //per carregar el fragment programàticament:
+    public static FragmentManager fragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //per carregar el Home_Fragment programàticament:
+        //inicialització del fragment manager:
+        //fragmentManager = getSupportFragmentManager();
+        fragmentManager = getSupportFragmentManager();
+        if (findViewById(R.id.fragment_container) == null) {
+        } else {
+            if (savedInstanceState != null) {
+                return;
+            }
+            fragmentManager.beginTransaction().add(R.id.fragment_container, new HomeFragment()).commit();
+        }
     }
 }
